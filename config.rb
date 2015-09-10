75fc034f6322c189ebc97d605ca0db97c28917d3
Diego R. Herrera <diegorherrera@gmail.com>
2015-09-10 15:50:22 +0100

    Setup Compass

---
+++ config.rb
@@ -0,0 +1,10 @@
+require 'compass/import-once/activate'
+http_path = "/"
+css_dir = "assets/css"
+sass_dir = "assets/scss"
+javascripts_dir = "assets/js"
+
+output_style = :compressed
+
+relative_assets = true
+line_comments = false
